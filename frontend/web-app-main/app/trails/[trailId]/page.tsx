e955aadb9d3605543dafc2ced7e2d15d3aa631be
Matt Huang <m223huan@uwaterloo.ca>
2026-03-04 16:44:19 -0500

    fix bug with trail pages not loading

--- frontend/web-app-main/app/trails/[trailId]/page.tsx
+++ frontend/web-app-main/app/trails/[trailId]/page.tsx
@@ -1,4 +1,11 @@
 import { TrailsApp } from "@/components/trails-app"
+import { staticTrails } from "@/lib/static-data"
+
+export function generateStaticParams() {
+  return staticTrails.map((trail) => ({
+    trailId: trail.id,
+  }))
+}
 
 export default function TrailPage() {
   return <TrailsApp />
